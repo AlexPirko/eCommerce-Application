d6e6fa0cc467573f817665aedacede30e22086aa
KirillVM <forjustice55@gmail.com>
2023-08-17 20:43:18 +0300

    refactor: delete semicolon

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');;
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
 const baseConfig = {
   entry: path.resolve(__dirname, './src/index'),
