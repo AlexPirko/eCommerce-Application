e79d5d0536ebf79f3c8176cc5762a7f7adf84fad
pozdnyakoks <pozdnyakoks@mail.ru>
2023-08-19 09:07:31 -0300

    try test

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,10 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   moduleNameMapper: {
-    '^@components/(.*)$': '<rootDir>/src/components/$1'
-  }
+    '^@components/(.ts)$': '<rootDir>/src/components/$1'
+  },
+  moduleDirectories: ['node_modules', 'src'],
+  modulePaths: [
+    "<rootDir>",
+  ]
 };
--- src/tests/input.test.ts
+++ src/tests/input.test.ts
@@ -1,43 +1,44 @@
-// // eslint-disable-next-line max-lines-per-function
-// describe('Input Class Tests', () => {
-//   let inputInstance: Input;
+import { Input } from '@components/common/input/Input';
+//  eslint-disable-next-line max-lines-per-function
+describe('Input Class Tests', () => {
+  let inputInstance: Input;
 
-//   beforeEach(() => {
-//     const mockInputData = {
-//       type: 'text',
-//       id: 1,
-//       classNames: ['input-field'],
-//       placeholder: 'Enter your name',
-//       value: 'John Doe',
-//     };
+  beforeEach(() => {
+    const mockInputData = {
+      type: 'text',
+      id: 1,
+      classNames: ['input-field'],
+      placeholder: 'Enter your name',
+      value: 'John Doe',
+    };
 
-//     inputInstance = new Input(mockInputData);
-//   });
+    inputInstance = new Input(mockInputData);
+  });
 
-//   // test('Input instance should be created correctly', () => {
-//   //   expect(inputInstance).toBeInstanceOf(Input);
-//   //   expect(inputInstance.type).toBe('text');
-//   //   expect(inputInstance.id).toBe(1);
-//   //   expect(inputInstance.classNames).toEqual(['input-field']);
-//   //   expect(inputInstance.placeholder).toBe('Enter your name');
-//   //   expect(inputInstance.value).toBe('John Doe');
-//   // });
+  // test('Input instance should be created correctly', () => {
+  //   expect(inputInstance).toBeInstanceOf(Input);
+  //   expect(inputInstance.type).toBe('text');
+  //   expect(inputInstance.id).toBe(1);
+  //   expect(inputInstance.classNames).toEqual(['input-field']);
+  //   expect(inputInstance.placeholder).toBe('Enter your name');
+  //   expect(inputInstance.value).toBe('John Doe');
+  // });
 
-//   test('getInputElement method should return an HTMLInputElement', () => {
-//     const inputElement = inputInstance.getInputElement();
-//     expect(inputElement).toBeInstanceOf(HTMLInputElement);
-//     expect(inputElement.type).toBe('text');
-//     expect(inputElement.id).toBe('1');
-//     expect(inputElement.placeholder).toBe('Enter your name');
-//     expect(inputElement.value).toBe('John Doe');
-//   });
+  test('getInputElement method should return an HTMLInputElement', () => {
+    const inputElement = inputInstance.getInputElement();
+    expect(inputElement).toBeInstanceOf(HTMLInputElement);
+    expect(inputElement.type).toBe('text');
+    expect(inputElement.id).toBe('1');
+    expect(inputElement.placeholder).toBe('Enter your name');
+    expect(inputElement.value).toBe('John Doe');
+  });
 
-//   test('createInputElement method should create and configure an input element', () => {
-//     const inputElement = inputInstance['createInputElement']();
-//     expect(inputElement).toBeInstanceOf(HTMLInputElement);
-//     expect(inputElement.type).toBe('text');
-//     expect(inputElement.id).toBe('1');
-//     expect(inputElement.placeholder).toBe('Enter your name');
-//     expect(inputElement.value).toBe('John Doe');
-//   });
-// });
+  test('createInputElement method should create and configure an input element', () => {
+    const inputElement = inputInstance['createInputElement']();
+    expect(inputElement).toBeInstanceOf(HTMLInputElement);
+    expect(inputElement.type).toBe('text');
+    expect(inputElement.id).toBe('1');
+    expect(inputElement.placeholder).toBe('Enter your name');
+    expect(inputElement.value).toBe('John Doe');
+  });
+});
