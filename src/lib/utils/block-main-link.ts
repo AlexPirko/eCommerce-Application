0d0fa5af7b686ebae01ace505618816627169526
pozdnyakoks <pozdnyakoks@mail.ru>
2023-08-21 09:31:51 -0300

    delete console.log

--- src/lib/utils/block-main-link.ts
+++ src/lib/utils/block-main-link.ts
@@ -2,8 +2,6 @@ export default function blockMainLink(): void {
   const isLogin: boolean = Boolean(localStorage.getItem('login')) || false;
   const signupMainLink: HTMLAnchorElement = document.querySelector('.main-signup_link') as HTMLAnchorElement;
   const loginMainLink: HTMLAnchorElement = document.querySelector('.main-login_link') as HTMLAnchorElement;
-  console.log(signupMainLink);
-  console.log(loginMainLink);
 
   if (isLogin && signupMainLink !== null && loginMainLink !== null) {
     signupMainLink.setAttribute('href', `http://${window.location.host}/main`);
