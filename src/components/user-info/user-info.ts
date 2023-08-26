77b2fa2c6f5b86953bc05bc6d319f17ec0c2e8ad
pozdnyakoks <pozdnyakoks@mail.ru>
2023-08-25 21:59:44 -0300

    feat: add user info block

--- src/components/user-info/user-info.ts
+++ src/components/user-info/user-info.ts
@@ -1,56 +1,111 @@
 import './user-info';
 import InputBlock from '@components/common/input/Input-block';
 import ApiServices from '@lib/api/api-services';
+import { Customer, ClientResponse } from '@commercetools/platform-sdk';
+import './user-info.scss';
 
+type TUserInfo = {
+  email?: string;
+  password?: string;
+  firstName?: string;
+  lastName?: string;
+  dateOfBirth?: string;
+};
 export class UserInfo {
-  createInfoBlock() {
-    const wrapper = document.createElement('div');
+  async createInfoBlock(): Promise<HTMLDivElement> {
+    const wrapper: HTMLDivElement = document.createElement('div');
     wrapper.classList.add('info-wrapper');
-    const title: HTMLHeadElement = document.createElement('h3');
-    title.textContent = 'User Info';
-    const btn = document.createElement('button');
-    btn.textContent = 'ggg';
-    const api = new ApiServices();
-    btn.addEventListener('click', async () => {
-      const res = await api.getCurrentCustomer();
+    const title: HTMLHeadElement = document.createElement('h5');
+    try {
+      title.textContent = 'User Info';
+      const res: Customer = await this.getInfo();
       console.log(res);
-    });
-    wrapper.append(title, this.createUserInfoBlock(), btn);
+      wrapper.append(title, this.createUserInfoBlock(res));
+      return wrapper;
+    } catch {
+      title.textContent = 'Something went wrong, please visit this page later';
+      wrapper.append(title);
+      return wrapper;
+    }
+  }
+
+  async getInfo(): Promise<Customer> {
+    const api: ApiServices = new ApiServices();
+    const res: ClientResponse<Customer> = await api.getCurrentCustomer();
+    return res.body;
+  }
+
+  // eslint-disable-next-line max-lines-per-function
+  createUserInfoBlock({ email, password, firstName, lastName, dateOfBirth }: TUserInfo): HTMLDivElement {
+    const wrapper: HTMLDivElement = document.createElement('div');
+    wrapper.classList.add('user-info');
+    if (email !== undefined) {
+      const emailInput: HTMLDivElement = new InputBlock({
+        type: 'email',
+        id: 'emailInput',
+        label: 'Email',
+        placeholder: '',
+        name: 'email',
+        value: email,
+        disabled: true,
+      }).create;
+      wrapper.append(emailInput);
+    }
+
+    if (password !== undefined) {
+      const passwordInput: HTMLDivElement = new InputBlock({
+        type: 'text',
+        id: 'passwordInput',
+        label: 'Password',
+        placeholder: '',
+        name: 'password',
+        value: '*****',
+        disabled: true,
+      }).create;
+      wrapper.append(passwordInput);
+    }
+    if (firstName !== undefined) {
+      const nameInput: HTMLDivElement = new InputBlock({
+        type: 'text',
+        id: 'nameInput',
+        label: 'First Name',
+        placeholder: '',
+        name: 'firstName',
+        value: firstName,
+        disabled: true,
+      }).create;
+      wrapper.append(nameInput);
+    }
+
+    if (lastName !== undefined) {
+      const surnameInput: HTMLDivElement = new InputBlock({
+        type: 'text',
+        id: 'surnameInput',
+        label: 'Second Name',
+        placeholder: '',
+        name: 'secondName',
+        value: lastName,
+        disabled: true,
+      }).create;
+      wrapper.append(surnameInput);
+    }
+    if (dateOfBirth !== undefined) {
+      const date = new Date(dateOfBirth);
+      const dateInput: HTMLDivElement = new InputBlock({
+        type: 'text',
+        id: 'dateOfBirth',
+        label: 'Date of Birth',
+        placeholder: '',
+        name: 'firstName',
+        value: date.toLocaleDateString('ru-RU'),
+        disabled: true,
+      }).create;
+      wrapper.append(dateInput);
+    }
     return wrapper;
   }
 
-  createUserInfoBlock(): DocumentFragment {
-    const wrapper: DocumentFragment = new DocumentFragment();
-    const nameInput: HTMLDivElement = new InputBlock({
-      type: 'text',
-      id: 'nameInput',
-      label: 'First Name',
-      placeholder: '',
-      name: 'firstName',
-      value: 'oks',
-      disabled: true,
-    }).create;
+  // createAddressBlock() {
 
-    const surnameInput: HTMLDivElement = new InputBlock({
-      type: 'text',
-      id: 'surnameInput',
-      label: 'Second Name',
-      placeholder: '',
-      name: 'secondName',
-      value: 'pozd',
-      disabled: true,
-    }).create;
-    const dateInput: HTMLDivElement = new InputBlock({
-      type: 'text',
-      id: 'nameInput',
-      label: 'First Name',
-      placeholder: '',
-      name: 'firstName',
-      value: '10.07.1994',
-      disabled: true,
-    }).create;
-
-    wrapper.append(nameInput, surnameInput, dateInput);
-    return wrapper;
-  }
+  // }
 }
--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,6 +1,7 @@
 import { Params } from '@lib/types/params-interface';
 import ComponentView from '@lib/services/component-view';
 import ElementBuilder from '@lib/services/element-builder';
+import { UserInfo } from '@components/user-info/user-info';
 
 export default class Profile extends ComponentView {
   constructor() {
@@ -14,14 +15,15 @@ export default class Profile extends ComponentView {
     this.configureView();
   }
 
-  private configureView(): void {
+  private async configureView(): Promise<void> {
     const titleParams: Params = {
       tagName: 'h2',
       classNames: ['profile-title'],
       text: 'Profile',
     };
     const titleElementBuilder: ElementBuilder = new ElementBuilder(titleParams);
-
+    const user = await new UserInfo().createInfoBlock();
     this.viewElementBuilder.addInnerElement(titleElementBuilder);
+    this.viewElementBuilder.addInnerElement(user);
   }
 }
