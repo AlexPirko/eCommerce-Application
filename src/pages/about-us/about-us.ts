07c328d022f8b7c0a643aa7d130bbcdded76f4e1
pozdnyakoks <pozdnyakoks@mail.ru>
2023-09-11 21:03:50 -0300

    feat: add about-us page

--- src/pages/about-us/about-us.ts
+++ src/pages/about-us/about-us.ts
@@ -3,6 +3,7 @@ import './about.scss';
 import { Params } from '@lib/types/params-interface';
 import ComponentView from '@lib/services/component-view';
 import ElementBuilder from '@lib/services/element-builder';
+import { AboutUs } from '@components/about-us/about-us';
 
 export default class About extends ComponentView {
   constructor() {
@@ -22,7 +23,9 @@ export default class About extends ComponentView {
       classNames: ['about_us-title'],
       text: 'About us',
     };
+    const aboutUs: HTMLDivElement = new AboutUs().createAboutPage();
     const titleElementBuilder: ElementBuilder = new ElementBuilder(titleParams);
     this.viewElementBuilder.addInnerElement(titleElementBuilder);
+    this.viewElementBuilder.addInnerElement(aboutUs);
   }
 }
